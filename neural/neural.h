7fab23655fd89b75b584fe76c6a191d2504622e9
Faraz Rahman <68860714+fpickle121@users.noreply.github.com>
2023-08-06 20:47:17 -0400

    Added Activation Layer functionality. Started process of backprop Some new linalg functionality for back prop

--- neural/neural.h
+++ neural/neural.h
@@ -5,34 +5,28 @@
 #include <string>
 
 #include "./../linalg/linalg.h"
-#include "activation.h"
 using std::string;
 
 class Layer {
-
+    
     protected:
-
         Layer* prevLayer;
         Layer* nextLayer;
 
     public:
         vector<int> shape;
-
         virtual void setPrevLayer(Layer* prevLayer);
         virtual void setNextLayer(Layer* nextLayer);
 
-
-        // if a specific class of Layers handles only 
-        // Matrices as input (like Dense layers) 
-        // then they should be individually declared 
-        // and defined in the Layer's name space
-        // virtual void feedForward(const Matrix& input); 
         /**
-         * @brief This method should be called by a Model object or 
-         * by this Layer's prevLayer. 
+         * @brief This method should be called by a Model 
+         * object or by this Layer's prevLayer. 
          * 
-         * input should therefore be the original input to the model 
-         * or the output of this Layer's prevLayer.
+         * NOTE: If a specific subclass of Layer handles only 
+         * Matrices objects - including column vectors - as input 
+         * (i.e. Dense layers) then the specific layer 
+         * should manually cast the Tensor to a Matrix and 
+         * catch for errors
          * 
          * @param input the input Tensor
          */
@@ -40,15 +34,12 @@ class Layer {
 
         /**
          * @brief This method should be called by a Model object or 
-         * this Layer's nextLayer). d will be the partial of error 
-         * with respect to this layer's output (nextLayer's input).
+         * this Layer's nextLayer. 
          * 
-         * @param d partial derivative to send backwards TO this layer 
-         * for back propagation. 
+         * @param delta should be the partial with respect to the 
+         * ouput Tensor of the layer backPropagate is being called from. 
          */
-        virtual void backPropagate(double d);
-
-
+        virtual void backPropagate(const Tensor& delta);
 };
 
 class InputLayer : public Layer {
@@ -57,6 +48,7 @@ class InputLayer : public Layer {
         InputLayer(vector<int> shape);
         void setPrevLayer(Layer* i_prevLayer) override;
         void feedForward(const Tensor& input) override;
+        void backPropagate(const Tensor& J_output) override;
 
 };
 
@@ -67,14 +59,14 @@ class OutputLayer : public Layer {
         OutputLayer();
         void setPrevLayer(Layer* i_prevLayer) override;
         void setNextLayer(Layer* i_nextLayer) override;
-        // void feedForward(const Matrix&) override;
         void feedForward(const Tensor&) override;
+        void backPropagate(const Tensor&) override;
         Tensor getLastOutput();
 
 };
 
 class FlattenLayer : public Layer {
-
+    
     public:
         /**
          * @brief Construct a new Flatten Layer object
@@ -90,6 +82,7 @@ class FlattenLayer : public Layer {
          */
         FlattenLayer();
         void feedForward(const Tensor& input) override;
+        void backPropagate(const Tensor& J_output) override;
         void setPrevLayer(Layer* i_prevLayer) override;
 
 };
@@ -106,14 +99,66 @@ class DenseLayer : public Layer {
     public: 
         DenseLayer(int i_size);
         void setPrevLayer(Layer* i_prevLayer) override;
-        // void feedForward(const Matrix& input); // this method must come first, so the compiler doesn't find the Tensor input method first
-        void feedForward(const Tensor& input) override; // this override catches the error of passing a Tensor into a DenseLayer
+        void feedForward(const Tensor& input) override;
 
 
 
 
 };
 
+class ActivationLayer : public Layer {
+
+    protected: 
+        string name;
+        Tensor (*activation)(const Tensor& input);
+        Tensor (*activationPrime)(const Tensor& input);   
+
+    public: 
+        
+        /**
+         * @brief Construct a new Activation Layer object
+         * using presets like "tanh" etc. a later feature 
+         * that might be worth adding is adjusting 
+         * distribution in last dense layer
+         * 
+         * NOTE: Currently implemented presets:
+         * "sigmoid",
+         * "tanh" 
+         * 
+         * @param name 
+         */
+        ActivationLayer(string name);
+
+        void feedForward(const Tensor& input) override;
+        // void backPropagate(const Tensor& J_output) override; 
+
+
+
+};
+
+class LossFunction {
+
+    protected:
+        string name;
+
+        Tensor (*loss)(const Tensor&, const Tensor&);
+        Tensor (*lossPrime)(const Tensor&, const Tensor&);
+
+    public:
+
+        /**
+         * @brief Construct a new Loss Function object.
+         * Similar to Activation Layers, you can pick
+         * from a set of preset loss functions
+         * 
+         * NOTE: Currently implemented presets:
+         * Mean Squared Error - "MSE",
+         * 
+         * @param i_name 
+         */
+        LossFunction(string i_name);
+
+};
 
 class Model {
 
@@ -121,6 +166,8 @@ class Model {
 
         InputLayer* inputLayer;
         OutputLayer* outputLayer;
+
+        LossFunction* loss;
 
     public: 
 
@@ -136,11 +183,24 @@ class Model {
         Model(vector<int> shape, vector<Layer*> hiddenLayers);
 
         /**
-         * @brief essentially calls feed forward on the input layer 
-         * and then returns the final output layer Tensor 
+         * @brief Calls feed forward on the input 
+         * layer and then returns the final output layer 
+         * Tensor 
          * 
+         * @param input represents input to the model or in
+         * this case the input to the model as a whole
          */
         Tensor feedForward(const Tensor& input);
+
+        /**
+         * @brief Calculates partial of Cost with respect
+         * to the output
+         * 
+         * @param J_output is the partial derivative of
+         * the cost function J with respect to the output
+         * of the layer that this is being called from.
+         */
+        Tensor backPropagate(const Tensor& J_output);
 
         
 
